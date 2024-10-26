26d554ea32a3288631a0d4adb9b047bc39e207ca
Надежда Коновалова <nadezhdalek@yandex.ru>
2024-10-26 23:48:22 +0800

    pre-cleaning the repository

--- searchengine/src/main/java/searchengine/exaptions/CrawlingOfPagesFailed.java
+++
@@ -1,9 +0,0 @@
-package searchengine.exaptions;
-
-public class CrawlingOfPagesFailed extends RuntimeException{
-
-    //Не удалось выполнить сканирование всех страниц, содержащихся на указанном сайте
-    public CrawlingOfPagesFailed(String error) {
-        super(error);
-    }
-}
